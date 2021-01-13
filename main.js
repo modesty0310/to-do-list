49a6f4de18c110e701f1216137e06c414e9e0204
modesty0310 <rlatprua4@gmail.com>
2021-01-13 13:26:19 +0900

    할일 추가,제거 기능 추가

---
+++ main.js
@@ -0,0 +1,46 @@
+const text = document.querySelector('.input');
+const addBtn = document.querySelector('.add-button');
+const items = document.querySelector('.items');
+const deleteBtn = document.querySelectorAll('.item-delete')
+
+addBtn.addEventListener('click', e=>{
+    onAdd();
+});
+
+items.addEventListener('click', e => {
+    const target = e.target.dataset.id;
+    if(target){
+        const deleted = document.querySelector(`.item-row[data-id='${target}']`)
+        deleted.remove();
+    }
+})
+
+
+function onAdd() {
+    if(!text.value){
+        return;
+    };
+    const toDo = text.value;
+    const item = createItem(toDo);
+    items.appendChild(item);
+    text.value = ''
+    text.focus();
+};
+
+let id = 0;
+
+function createItem (toDo) {
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item-row')
+    itemRow.setAttribute('data-id', id)
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item-name">${toDo}</span>
+        <button class="item-delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    `
+    id++;
+    return itemRow;
+};
